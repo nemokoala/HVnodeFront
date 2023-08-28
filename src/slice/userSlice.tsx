4e0169a5d5dceeffaa9ff451738cbd6eb359f4d4
nemokoala <fdxguy2@gmail.com>
2023-08-28 22:10:40 +0900

    test

--- src/slice/userSlice.tsx
+++ src/slice/userSlice.tsx
@@ -9,9 +9,10 @@ export const userSlice = createSlice({
     saveSession: (state, action: any): any => {
       console.log("reducer : " + JSON.stringify(action.payload));
       const data = { ...action.payload, date: now };
-      localStorage.setItem("session", JSON.stringify(data)); //sessionstoarge에 저장
+      alert("slice " + action.payload);
+      if (!action.payload.app)
+        localStorage.setItem("session", JSON.stringify(data)); //sessionstoarge에 저장
       if (action.payload === "") localStorage.removeItem("session"); // 값이 비면 세선 삭제
-      if (action.payload.app) localStorage.removeItem("session");
       state.session = action.payload;
     },
   },
