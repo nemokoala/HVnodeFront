bf17e6524692857b290baf8754828cc5413a07f2
nemokoala <fdxguy2@gmail.com>
2023-08-28 21:19:54 +0900

    수정

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -62,7 +62,8 @@ function App() {
 
       if (session.id === 0) {
         dispatch(saveSession("" as any)); //앱에서 로그인 상태가 아닐 시
-      } else if (session.id !== null) dispatch(saveSession(session as any));
+      } else if (session.id !== null)
+        dispatch(saveSession({ ...session } as any));
       console.log(session);
 
       try {
