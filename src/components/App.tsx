ee5e4af29b6719036de723a46a26dbfa9ff25b95
nemokoala <fdxguy2@gmail.com>
2023-08-28 20:12:54 +0900

    json parse 해서 받기

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -55,7 +55,7 @@ function App() {
   //웹뷰에서 세션 데이터 받기
   useEffect(() => {
     const handleMessage = async (event: any) => {
-      const session = event.data;
+      const session = JSON.parse(event.data);
       if (session.id !== null) dispatch(saveSession(session as any));
       if (session.id === 0) dispatch(saveSession("" as any)); //앱에서 로그인 상태가 아닐 시
       console.log(session);
@@ -72,7 +72,9 @@ function App() {
       }
     };
 
-    //(window as any).ReactNativeWebView.postMessage("번갑");
+    //   if (typeof (window as any).ReactNativeWebView !== "undefined") {
+    //     (window as any).ReactNativeWebView.postMessage("번갑");
+    // }
 
     window.addEventListener("message", handleMessage);
 
